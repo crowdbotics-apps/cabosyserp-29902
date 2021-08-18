1ec2bdcbc1d9e0d5967d7404a543c9827fe99849
Crowdbotics <team@crowdbotics.com>
2021-08-18 03:18:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cabosyserp_29902Identifier",
+  url: "https://cabosyserp_29902Identifier.botics.co",
+  api: "https://cabosyserp_29902Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
